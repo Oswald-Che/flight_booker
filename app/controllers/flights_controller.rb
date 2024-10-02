cde4ebb66c5a47b8bf3d3464ea62f8a59989fd28
Oswald-Che <cheoswald@yahoo.com>
2024-10-02 09:39:57 +0100

    Create search form for flights and render query results in index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,11 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
+    @passengers = (1..4).map { |i| [i, i] }
+    @flight_dates = Flight.all.map(&:start_time_formatted).uniq
+
+    if params[:departure_airport].present?
+      @selected_flights = Flight.where(departure_airport_id: params[:departure_airport], arrival_airport: params[:arrival_airport])
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-%w[ATL DEN SFO NYC LAX IST JFK AMS MAD YYZ].each do |code|
+airport_code = %w[ATL DEN SFO NYC]
+airport_code.each do |code|
   Airport.create(code:)
 end
 
-def num(number)
-  number + 60
+100.times do
+  airports = airport_code.sample(2)
+  arriving_airport = Airport.find_by(code: airports[0]).id
+  departure_airport = Airport.find_by(code: airports[1]).id
+  time = rand(4.days.from_now..15.days.from_now)
+  duration = rand(1..30)
+  Flight.create(departure_airport_id: departure_airport,
+                arrival_airport_id: arriving_airport,
+                start_time: time, duration: "#{duration} hours")
 end
-
-Flight.create(departure_airport_id: num(1), arrival_airport_id: num(4), start_time: DateTime.parse("01/10/2024 08:00"), duration: "5 hours")
-Flight.create(departure_airport_id: num(2), arrival_airport_id: num(4), start_time: DateTime.parse("02/10/2024 10:00"), duration: "3 hours")
-Flight.create(departure_airport_id: num(4), arrival_airport_id: num(9), start_time: DateTime.parse("04/10/2024 18:50"), duration: "21 hours")
-Flight.create(departure_airport_id: num(5), arrival_airport_id: num(7), start_time: DateTime.parse("10/10/2024 01:00"), duration: "6 hours")
-Flight.create(departure_airport_id: num(3), arrival_airport_id: num(10), start_time: DateTime.parse("06/11/2024 04:30"), duration: "9 hours")
-Flight.create(departure_airport_id: num(4), arrival_airport_id: num(8), start_time: DateTime.parse("11/10/2024 15:00"), duration: "29 hours")
-Flight.create(departure_airport_id: num(5), arrival_airport_id: num(3), start_time: DateTime.parse("08/10/2024 17:00"), duration: "7 hours")
-Flight.create(departure_airport_id: num(8), arrival_airport_id: num(9), start_time: DateTime.parse("12/10/2024 04:40"), duration: "8 hours")
-Flight.create(departure_airport_id: num(10), arrival_airport_id: num(2), start_time: DateTime.parse("01/11/2024 06:13"), duration: "4 hours")
-Flight.create(departure_airport_id: num(1), arrival_airport_id: num(6), start_time: DateTime.parse("21/10/2024 08:00"), duration: "22 hours")
