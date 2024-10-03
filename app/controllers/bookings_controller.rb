8f86438929f7b87324a2c36e69752d25f3803e1a
Oswald-Che <cheoswald@yahoo.com>
2024-10-03 19:34:27 +0100

    Add create action in bookings controller to create new bookings and passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,10 +2,19 @@ class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight])
     @booking = @flight.bookings.build
-    @passengers = @booking.passengers.build(flight_id: @flight.id)
     @passenger_amount = params[:passenger].to_i
+    @passenger_amount.times { @booking.passengers.build(flight_id: @flight.id) }
   end
 
   def create
+    id = params[:booking][:passengers_attributes]["0"][:flight_id]
+    @flight = Flight.find(id)
+    @booking = @flight.bookings.create!(booking_params)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:passenger_amount, passengers_attributes: [:name, :email, :flight_id])
   end
 end
