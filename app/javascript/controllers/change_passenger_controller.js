bc757443a594251d43c7e1b7272883016e667222
Oswald-Che <cheoswald@yahoo.com>
2024-10-17 14:54:00 +0100

    fix stimulus action to add passenger and create new passengers

--- app/javascript/controllers/change_passenger_controller.js
+++ app/javascript/controllers/change_passenger_controller.js
@@ -2,10 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-passenger"
 export default class extends Controller {
-    static targets = ["template", 'output']
+    static targets = ["template", "output", "node"]
   addPassenger() {
-    console.log(this.hasOutputTarget)
-    this.outputTarget.appendChild(this.templateTarget.content)
-
+    console.log(this.hasTemplateTarget)
+    
+    this.clone = this.templateTarget.content.cloneNode(true)
+    this.outputTarget.appendChild(this.clone)
+    console.log(this.nodeTargets)
   }
 }
