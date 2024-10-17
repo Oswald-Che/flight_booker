f716c92f573ca87a74167c55e8a779771f860ee6
Oswald-Che <cheoswald@yahoo.com>
2024-10-17 19:43:26 +0100

    Add confirmation email action in mailer with views

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    mail(to: @user.email, subject: 'Congratulation on booking a flight')
+  end
 end
