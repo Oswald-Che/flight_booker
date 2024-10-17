1d5153692e55b9b6bc87cf57ef4dd6a316eadd62
Oswald-Che <cheoswald@yahoo.com>
2024-10-17 16:26:48 +0100

    Add remove passenger button to remove passengers in passenger form

--- app/javascript/controllers/change_passenger_controller.js
+++ app/javascript/controllers/change_passenger_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-passenger"
 export default class extends Controller {
-    static targets = ["template", "output", "node"]
-  addPassenger() {
-    console.log(this.hasTemplateTarget)
+    static targets = ["template", "output", "node", "context"]
+    static values = {count: Number}
     
-    this.clone = this.templateTarget.content.cloneNode(true)
-    this.outputTarget.appendChild(this.clone)
-    console.log(this.nodeTargets)
-  }
+    addPassenger() {
+        this.clone = this.templateTarget.content.cloneNode(true)
+        this.outputTarget.appendChild(this.clone)
+        this.countValue++
+    }
+
+    removePassenger() {
+        if(this.countValue > 1){
+            this.lastNode = this.nodeTargets.pop()
+            this.outputTarget.removeChild(this.lastNode)
+            this.countValue--
+        }    
+       
+    }
+
+ 
 }
